00994103dc12a57beb50e11eff7ae5e8f699ee0a
willy68 <william.lety@gmail.com>
2023-03-27 18:17:13 +0200

    Initial commit

--- tests/ComposerInstaller/ModuleInstallerTests.php
+++ tests/ComposerInstaller/ModuleInstallerTests.php
@@ -4,9 +4,115 @@ declare(strict_types=1);
 
 namespace ComposerInstaller;
 
+use Composer\Composer;
+use Composer\Config;
+use Composer\IO\IOInterface;
+use Composer\Package\Package;
+use Composer\Repository\RepositoryManager;
+use Composer\Util\HttpDownloader;
+use PgFramework\ComposerInstaller\ModuleInstaller;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class ModuleInstallerTests extends TestCase
 {
+
+    /**
+     * @var Composer
+     */
+    protected Composer $composer;
+
+    /**
+     * @var Package
+     */
+    protected Package $package;
+
+    /**
+     * @var IOInterface&MockObject
+     */
+    protected $io;
+
+    /**
+     * @var ModuleInstaller
+     */
+    protected ModuleInstaller $plugin;
+
+    /**
+     * Directories used during tests
+     *
+     * @var array
+     */
+    protected array $testDirs = [
+        '',
+        'vendor',
+        'plugins',
+        'plugins/Foo',
+        'plugins/Fee',
+        'plugins/Foe',
+        'plugins/Fum',
+        'app_plugins',
+        'app_plugins/Bar',
+    ];
+
+    /**
+     * setUp
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->package = new Package('cake/plugin', '1.0', '1.0');
+        $this->package->setType('cakephp-plugin');
+
+        $this->path = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'module-installer-test';
+
+        foreach ($this->testDirs as $dir) {
+            if (!is_dir($this->path . '/' . $dir)) {
+                mkdir($this->path . '/' . $dir);
+            }
+        }
+
+        $this->composer = new Composer();
+        $config = new Config();
+        $config->merge([
+            'vendor-dir' => $this->path . '/vendor',
+        ]);
+
+        $this->composer->setConfig($config);
+
+        /** @var IOInterface&MockObject $io */
+        $io = $this->getMockBuilder(IOInterface::class)->getMock();
+        $this->io = $io;
+
+        $httpDownloader = new HttpDownloader($this->io, $config);
+
+        $rm = new RepositoryManager(
+            $this->io,
+            $config,
+            $httpDownloader
+        );
+        $this->composer->setRepositoryManager($rm);
+
+        $this->plugin = new ModuleInstaller();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        $dirs = array_reverse($this->testDirs);
+
+        if (is_file($this->path . '/vendor/cakephp-plugins.php')) {
+            unlink($this->path . '/vendor/cakephp-plugins.php');
+        }
+
+        foreach ($dirs as $dir) {
+            if (is_dir($this->path . '/' . $dir)) {
+                rmdir($this->path . '/' . $dir);
+            }
+        }
+    }
 
 }
